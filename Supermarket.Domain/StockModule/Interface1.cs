9243f6fad377a6d00aeffc774aee445bb36bbf97
Victor R. Silva <victorr.silva005@gmail.com>
2025-07-15 14:40:01 -0300

    Criado módulo de Estoque

---
+++ Supermarket.Domain/StockModule/Interface1.cs
@@ -0,0 +1,6 @@
+﻿using Supermarket.Domain.Shared;
+
+namespace Supermarket.Domain.StockModule;
+public interface IStockModuleRepository : IRepository<Tower>
+{
+}
---
+++ Supermarket.Domain/StockModule/Tower.cs
@@ -0,0 +1,23 @@
+﻿using Supermarket.Domain.Shared;
+
+namespace Supermarket.Domain.StockModule;
+public class Tower : BaseEntity<Tower>
+{
+    public string TowerName { get; set; }
+    public List<Shelf> Shelves { get; set; }
+
+    public Tower()
+    {
+        Shelves = new List<Shelf>();
+    }
+
+    public Tower(string towerName, List<Shelf> shelves) : this()
+    {
+        TowerName = towerName;
+        Shelves = shelves;
+    }
+    public override void UpdateRecord(Tower UpdatedRecord)
+    {
+        throw new NotImplementedException();
+    }
+}
