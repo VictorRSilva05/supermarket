a7eb1b3af8ca5b6f87066f4846662cea39156da8
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-11 09:05:20 -0300

    Implementado módulo Supplier em Orm

---
+++ Supermarket.Infraestructure.Orm/SupplierModule/SupplierConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Supermarket.Domain.SupplierModule;
+
+namespace Supermarket.Infraestructure.Orm.SupplierModule;
+public class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+{
+    public void Configure(EntityTypeBuilder<Supplier> builder)
+    {
+        builder.Property(d => d.Id)
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(d => d.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(d => d.Phone)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        builder.Property(d => d.Ein)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        builder.HasOne(d => d.Address)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Cascade);      
+    }
+}
---
+++ Supermarket.Infraestructure.Orm/SupplierModule/SupplierRepository.cs
@@ -0,0 +1,11 @@
+﻿using Supermarket.Domain.SupplierModule;
+using Supermarket.Infraestructure.Orm.Shared;
+
+namespace Supermarket.Infraestructure.Orm.SupplierModule;
+public class SupplierRepository : BaseOrmRepository<Supplier>, ISupplierRepository
+{
+    public SupplierRepository(SupermarketDbContext context) : base(context)
+    {
+    }
+}
+
