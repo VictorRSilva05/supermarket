c6d29353ac9dffbc44d0f9116039c5649fa1c33b
Victor R. Silva <victorr.silva005@gmail.com>
2025-08-18 19:49:36 -0300

    Criado módulo Batch em Orm

---
+++ Supermarket.Infraestructure.Orm/BatchFolder/BatchConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Supermarket.Domain.BatchModule;
+
+namespace Supermarket.Infraestructure.Orm.BatchFolder;
+public class BatchConfiguration : IEntityTypeConfiguration<Batch>
+{
+    public void Configure(EntityTypeBuilder<Batch> builder)
+    {
+        builder.Property(d => d.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(d => d.BatchNumber)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(d => d.Product)
+            .IsRequired();
+
+        builder.Property(d => d.Quantity)
+            .IsRequired();
+
+        builder.Property(d => d.ManufactureDate)
+            .IsRequired()
+            .HasColumnType("datetime");
+
+        builder.Property(d => d.ExpiryDate)
+            .IsRequired()
+            .HasColumnType("datetime");
+
+        builder.Property(d => d.Supplier)
+            .IsRequired();
+
+        builder.Property(d => d.UnitPrice)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Property(d => d.TotalPrice)
+            .HasComputedColumnSql("[UnitPrice] * [Quantity]")
+            .IsRequired();
+    }
+}
---
+++ Supermarket.Infraestructure.Orm/BatchFolder/BatchRepository.cs
@@ -0,0 +1,11 @@
+﻿using Supermarket.Domain.BatchModule;
+using Supermarket.Infraestructure.Orm.Shared;
+
+namespace Supermarket.Infraestructure.Orm.BatchFolder;
+public class BatchRepository : BaseOrmRepository<Batch>, IBatchRepository
+{
+    public BatchRepository(SupermarketDbContext context) : base(context)
+    {
+    }
+}
+
